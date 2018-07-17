19dcf0503f3eb76a60587049786a127dc7000311
Shengxiang <dannyzhou97@gmail.com>
2018-07-17 16:57:36 -0400

    update array for comments

--- script.js
+++ script.js
@@ -1,41 +1,49 @@
-async function sampleTransaction(tx) {  // eslint-disable-line no-unused-vars
-    console.log('Comment Submitted:'+ tx.newComment);
-    // Save the old value of the asset.
-    var oldComment = "none";
-    if (!tx.doc.comment){
+/**
+ * Sample transaction processor function.
+ * @param {org.docnetwork.basic.updateComment} ct The sample transaction instance.
+ * @transaction
+ */
+async function updateComment(ct) {  // eslint-disable-line no-unused-vars
+  var oldct;
+  console.log('Comment Submitted:'+ ct.comment);
+  // Update the asset with the new value
+    if (ct.doc.upct){
+      const oldComment = ct.doc.upct[ct.doc.upct.length - 1];
+      oldct = oldComment.comment;
+      ct.doc.upct.push(ct);
     }else{
-        oldComment = tx.doc.comment;
+      ct.doc.upct = [ct];
+      oldct = "none";
     }
-    // Update the asset with the new value.
-    tx.doc.comment = tx.newComment;
-    
-    if (!tx.doc.version){
-        var oldVersion = 0;
-        tx.doc.version = 1;
+
+    if (!ct.doc.version){
+      var oldVersion = 0;
+      ct.doc.version = 1;
     }else{
-        oldVersion = tx.doc.version;
-        tx.doc.version ++;
+      oldVersion = ct.doc.version;
+      ct.doc.version ++;
     }
     
-    var estTime = new Date().toLocaleString("en-US", {timeZone: "America/New_York"})
-    tx.doc.lastmodifiedtime = estTime;
+  var estTime = new Date().toLocaleString("en-US", {timeZone: "America/New_York"})
+  ct.doc.lastmodifiedtime = estTime;
     
-    // Get the asset registry for the asset.
-    const assetRegistry = await getAssetRegistry('org.example.basic.Document');
-    // Update the asset in the asset registry.
-    await assetRegistry.update(tx.doc);
-    console.log('Comment Updated');
-    console.log(estTime); //Gives Mon Mar 21 2016 23:00:00 GMT+0530 (IST)
-    
-    // Emit an event for the modified asset.
-    let event = getFactory().newEvent('org.example.basic', 'SampleEvent');
-    event.doc = tx.doc;
-    event.oldComment = oldComment;
-    event.newComment = tx.newComment;
-    event.oldVersion = oldVersion;
-    event.newVersion = tx.doc.version;
-    emit(event);
+  // Get the asset registry for the asset.
+  const assetRegistry = await getAssetRegistry('org.docnetwork.basic.Document');
+  // Update the asset in the asset registry.
+  await assetRegistry.update(ct.doc);
+  console.log('Comment Updated');
+  console.log(estTime);
+
+  // Emit an event for the modified asset.
+  let event = getFactory().newEvent('org.docnetwork.basic', 'SampleEvent');
+  event.doc = ct.doc;
+  event.oldComment = oldct;
+  event.newComment = ct.comment;
+  event.oldVersion = oldVersion;
+  event.newVersion = ct.doc.version;
+  emit(event);
+
 }
 async function setupDemo(setupDemo) {  // eslint-disable-line no-unused-vars
-  
+
 }
